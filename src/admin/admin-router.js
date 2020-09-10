346e85647a3688e92963035a0e808c1cccd92f11
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-10 00:37:30 -0400

    added routes and services

--- src/admin/admin-router.js
+++ src/admin/admin-router.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const AdminService = require('./admin-service');
 const path = require('path');
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const adminRouter = express.Router();
 const jsonBodyParser = express.json();
 
+adminRouter.route("/")
+    .all(requireAuth)
+    .get((req, res, next) => {
+        AdminService.getAllUsers(req.app.get('db'))
+        .then(users => {
+            res
+            .status(200)
+            .json(users)
+        })
+        .catch(next)
+    })
+    .delete(bodyParser,(req,res,next) => {
+        const { id, email } = req.params;
+
+        AdminService.deleteUser(req.app.get("db"), id, email)
+            .then(() => {
+                res.status(204);
+            })
+            .catch(next);
+    });
+
+module.exports = adminRouter;
--- src/admin/admin-service.js
+++ src/admin/admin-service.js
@@ -3,19 +3,20 @@ const xss = require('xss');
 const bcrypt = require('bcryptjs');
 
 const AdminService = {
-    validatePassword(password) {
-        if (password.length < 8) {
-          return 'Password must be longer than 8 characters';
-        }
-        if (password.length > 72) {
-          return 'Password must be shorter than 72 characters';
-        }
-        if (password.startsWith(' ') || password.endsWith(' ')) {
-          return 'Password must not start or end with empty spaces';
-        }
-        if (!REGEX_UPPER_LOWER_NUMBER.test(password)) {
-          return 'Password must contain an uppercase, lowercase, and number';
-        }
-        return null;
+    getAllUsers(db) {
+      return db
+      .select(
+        '*'
+      )
+      .from('users')
+    },
+
+    deleteUser(db, id, user_email) {
+      return db('users')
+      .where(
+        db.raw(`id=${id}
+        and email=${user_email}`)
+      )
+      .delete();
     }
 }
